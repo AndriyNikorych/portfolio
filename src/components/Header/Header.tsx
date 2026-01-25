6bbaeef5d212a7dab1c727ed1d86ad68ca663305
NikorychAndriy <anikorych@codifire.com>
2026-01-26 01:23:39 +0200

    fix routes for git without middleware

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,9 @@
 "use client";
 import css from "@css/component/header.module.scss";
-import Link from "next/dist/client/link";
 import HomeIcon from "@/assets/svg/home.svg";
 import { Settings } from "@/components/Header/Settings";
 import { useTranslation } from "@/locale/useTranslation";
 import { NavigationItem } from "@/components/Header/NavigationItem";
-import { LiquidGlass } from "@/components/Liquid Glass";
 
 export function Header() {
 	const t = useTranslation();
@@ -15,10 +13,10 @@ export function Header() {
 			<div className={css.headerContent}>
 				<nav className={css.navigation}>
 					<ul className={css.menu}>
-						<NavigationItem url={"/"} icon={<HomeIcon />} />
-						<NavigationItem url={"/about"} title={t("navigation.about")} />
-						<NavigationItem url={"/projects"} title={t("navigation.projects")} />
-						<NavigationItem url={"/contacts"} title={t("navigation.contacts")} />
+						<NavigationItem url={"/${locale}"} icon={<HomeIcon />} />
+						<NavigationItem url={"/${locale}/about"} title={t("navigation.about")} />
+						<NavigationItem url={"/${locale}/projects"} title={t("navigation.projects")} />
+						<NavigationItem url={"/${locale}/contacts"} title={t("navigation.contacts")} />
 					</ul>
 				</nav>
 				<Settings />
