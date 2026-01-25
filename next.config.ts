d64c4d444f6c6f56c2cb32f36bdbe810fbbd2c50
NikorychAndriy <anikorych@codifire.com>
2026-01-26 00:16:15 +0200

    fix layout

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/locale/i18n/request.ts");
 
+const repo = "portfolio";
+
 const nextConfig: NextConfig = {
+	output: "export",
+	basePath: `/${repo}`,
+	assetPrefix: `/${repo}/`,
+	images: { unoptimized: true },
 	turbopack: {
 		rules: {
 			"*.svg": {
