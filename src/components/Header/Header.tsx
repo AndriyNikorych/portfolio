b4fb13c1858f93de64885138d88c13a75e8ffab9
NikorychAndriy <anikorych@codifire.com>
2026-01-26 01:33:49 +0200

    add locale to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,19 +4,21 @@ import HomeIcon from "@/assets/svg/home.svg";
 import { Settings } from "@/components/Header/Settings";
 import { useTranslation } from "@/locale/useTranslation";
 import { NavigationItem } from "@/components/Header/NavigationItem";
+import { useLocale } from "use-intl";
 
 export function Header() {
 	const t = useTranslation();
+	const locale = useLocale();
 
 	return (
 		<header className={css.header}>
 			<div className={css.headerContent}>
 				<nav className={css.navigation}>
 					<ul className={css.menu}>
-						<NavigationItem url={"/${locale}"} icon={<HomeIcon />} />
-						<NavigationItem url={"/${locale}/about"} title={t("navigation.about")} />
-						<NavigationItem url={"/${locale}/projects"} title={t("navigation.projects")} />
-						<NavigationItem url={"/${locale}/contacts"} title={t("navigation.contacts")} />
+						<NavigationItem url={`/${locale}`} icon={<HomeIcon />} />
+						<NavigationItem url={`/${locale}/about`} title={t("navigation.about")} />
+						<NavigationItem url={`/${locale}/projects`} title={t("navigation.projects")} />
+						<NavigationItem url={`/${locale}/contacts`} title={t("navigation.contacts")} />
 					</ul>
 				</nav>
 				<Settings />
